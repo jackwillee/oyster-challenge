543dbfb63d7e786d0b86d45717a519a33e6127b8
basselalsayed <alsayed.bassel@gmail.com>
2019-12-17 16:27:16 +0000

    chapter 12

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -2,27 +2,32 @@ class Oystercard
   MAXIMUM_BALANCE = 90
   MINIMUM_BALANCE = 1 
   
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :journeys
 
   def initialize
-  @balance = 0
+    @balance = 0
+    @journeys = []
 
-end 
+  end 
 
 def top_up(amount)
   raise "balance exceeded" if @balance + amount > MAXIMUM_BALANCE
   @balance = @balance + amount
 end 
 
-
-
-def touch_in(station)
-    raise "insufficient funds" if @balance  < MINIMUM_BALANCE
-  @entry_station = station 
+def touch_in(entry_station)
+  raise "insufficient funds" if @balance  < MINIMUM_BALANCE
+  @entry_station = entry_station 
+  @journey = Hash.new
+  @journey["#{@entry_station}"] = nil
 end 
-def touch_out
-  @entry_station = nil 
+
+def touch_out(exit_station)
+  
   deduct
+  @journey["#{@entry_station}"] = "#{exit_station}"
+  @journeys << @journey
+  @entry_station = nil 
 end 
 
 def in_journey?
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,9 +1,18 @@
 require 'oystercard'
 describe Oystercard do
-let(:station) {double(:station)}
-  it 'has a balance of zero' do
-      expect(subject.balance).to eq(0)
-  end 
+
+  let(:station) {double(:station)}
+  let(:station2) {double(:station2)}
+  
+  describe '#initialization' do
+    it 'has a balance of zero' do
+        expect(subject.balance).to eq(0)
+    end 
+    it 'has an empty list of journeys' do
+      expect(subject.journeys).to eq []
+    end
+  end
+  
   describe '#top_up' do 
     it { is_expected.to respond_to(:top_up).with(Oystercard::MINIMUM_BALANCE).argument }
      
@@ -15,43 +24,51 @@ let(:station) {double(:station)}
       expect{ subject.top_up(Oystercard::MAXIMUM_BALANCE + Oystercard::MINIMUM_BALANCE) }.to raise_error "balance exceeded"
     end 
   end 
- 
-describe '#deduct' do 
+   
+  describe '#deduct' do 
   xit "deducts money" do 
     subject.top_up(10)
     expect{ subject.deduct }.to change{ subject.balance }.by(- Oystercard::MINIMUM_BALANCE)
   end 
-end 
-describe '#touch_in' do 
-  it "touches in" do 
-    subject.top_up(5)
-    subject.touch_in(station)
-    expect(subject.in_journey?).to eq(true)
-  end
-  it "touches in" do 
-    expect{subject.touch_in(station)}.to raise_error "insufficient funds"
   end 
-it "remembers the station" do 
-  subject.top_up(5)
-    subject.touch_in(station)
-    expect(subject.entry_station).to eq(station)
-end 
-
-end
-describe '#touch_out' do 
-  it "touches out" do 
-    subject.top_up(5)
-    subject.touch_in(station)
-    subject.touch_out
-    expect(subject.in_journey?).to eq(false)
-end
-  it 'deducts balance' do
-    subject.top_up(5)
-    subject.touch_in(station)
-    expect{ subject.touch_out }.to change{ subject.balance }.by(- Oystercard::MINIMUM_BALANCE)
+  
+  describe '#touch_in' do 
+    it "touches in" do 
+      subject.top_up(5)
+      subject.touch_in(station)
+      expect(subject.in_journey?).to eq(true)
+    end
+    it "touches in" do 
+      expect{subject.touch_in(station)}.to raise_error "insufficient funds"
+    end 
+    it "remembers the station" do 
+      subject.top_up(5)
+        subject.touch_in(station)
+        expect(subject.entry_station).to eq(station)
+    end 
+  
   end
- 
-end
+
+  describe '#touch_out' do 
+    it "touches out" do 
+      subject.top_up(5)
+      subject.touch_in(station)
+      subject.touch_out(station2)
+      expect(subject.in_journey?).to eq(false)
+    end
+    it 'deducts balance' do
+      subject.top_up(5)
+      subject.touch_in(station)
+      expect{ subject.touch_out(station2) }.to change{ subject.balance }.by(- Oystercard::MINIMUM_BALANCE)
+    end
+   it 'creates one journey' do
+     subject.top_up(5)
+     subject.touch_in(station)
+     subject.touch_out(station2)
+     p subject.journeys
+     expect(subject.journeys[0]).to eq ({"#{station}" => "#{station2}"})
+   end
+  end
 
 
 
